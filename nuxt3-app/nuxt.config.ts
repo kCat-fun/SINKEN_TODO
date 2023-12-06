ba18bc7958043ce4d550cc594f0dc1019bc4ac79
kCat-fun <kcat.fun.stu@gmail.com>
2023-12-06 15:59:02 +0900

    setting nuxt3-app

--- nuxt3-app/nuxt.config.ts
+++ nuxt3-app/nuxt.config.ts
@@ -1,4 +1,48 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      charset: "utf-16",
+      viewport: "width=device-width",
+      title: "Todo SINKEN",
+      meta: [
+        // <meta name="description" content="My amazing site.">
+        {
+          name: "description",
+          content:
+            "Todoアプリ。SINKEN疑似ハッカソン用。",
+        },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css?family=Inter",
+        },
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico",
+        },
+      ],
+      script: [],
+    },
+  },
+  css: [
+    "~/assets/style/reset.css",
+    "@fortawesome/fontawesome-svg-core/styles.css",
+  ],
+  components: [
+    {
+      path: "~/components/",
+      pathPrefix: false,
+    },
+  ],
+  build: {
+    transpile: [
+      "@fortawesome/fontawesome-svg-core",
+      "@fortawesome/free-solid-svg-icons",
+      "@fortawesome/vue-fontawesome",
+    ],
+  },
+});
---
+++ nuxt3-app/pages/index.vue
@@ -0,0 +1,13 @@
+<!-- ここがTodoのページ -->
+
+<template>
+<main>
+    <h1>Todo App</h1>
+</main>
+</template>
+
+<script>
+
+</script>
+
+<style src="./index.css" scoped></style>
