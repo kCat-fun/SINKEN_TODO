f36176499b8809526c363241c7ce792adeb1fbcf
kCat-fun <kcat.fun.stu@gmail.com>
2023-12-07 19:18:17 +0900

    TaskBoxをSampleComponentに追加

--- nuxt3-app/pages/ComponentTest/index.vue
+++ nuxt3-app/pages/ComponentTest/index.vue
@@ -1,12 +1,19 @@
 <template>
     <div id="ComponentTest">
         <Sample>tag</Sample>
+        <hr />
         <SampleBox name="未来 太郎" :tags=arr>Title</SampleBox>
         <div class="StateTag">
             <StateTag :isDone="false" />
             <StateTag :isDone="true" />
         </div>
+        <hr />
         <MoveToggle />
+        <hr />
+        <div class="TsakBox">
+            <TaskBox :isDone="false" dedline="20yy/mm/dd hh:mm:ss" taskTitle="ToDo Test1"></TaskBox>
+            <TaskBox :isDone="true" dedline="20yy/mm/dd hh:mm:ss" taskTitle="ToDo Test2"></TaskBox>
+        </div>
     </div>
 </template>
 
