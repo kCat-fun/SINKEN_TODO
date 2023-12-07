f42c5b41fcf0022fb6b271a3bb721e8039a6fda5
kCat-fun <kcat.fun.stu@gmail.com>
2023-12-07 17:54:05 +0900

    StateTagを追加

--- nuxt3-app/pages/ComponentTest/index.vue
+++ nuxt3-app/pages/ComponentTest/index.vue
@@ -2,18 +2,22 @@
     <div id="ComponentTest">
         <Sample>tag</Sample>
         <SampleBox name="未来 太郎" :tags=arr>Title</SampleBox>
+        <div class="StateTag">
+            <StateTag :isDone="false" />
+            <StateTag :isDone="true" />
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "ComponentTest",
-        data: () => {
-            return {
-                arr: ["tag1", "tag2", "tag3"],
-            }
+export default {
+    name: "ComponentTest",
+    data: () => {
+        return {
+            arr: ["tag1", "tag2", "tag3"],
         }
     }
+}
 </script>
 
 <style src="./index.css"></style>
