d96fed4efa69a49651efcbd287900d387441420f
b1022054 <b1022054@fun.ac.jp>
2023-12-09 05:46:55 +0900

    ログイン時エラーメッセージ処理の追加

---
+++ nuxt3-app/components/atoms/ErrorMessage/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <p v-if="message" class="error-message">{{ message }}</p>
+</template>
+  
+<script>
+export default {
+    props: {
+        message: String
+    }
+};
+</script>
+
+<style src="./index.css" scoped></style>
